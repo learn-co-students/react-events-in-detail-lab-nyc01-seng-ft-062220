83833f7543aaafea389fee789612461dc2e01bc3
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-08-24 17:29:40 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,39 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+class DelayedButton extends React.Component {
+    
+//got really close to the answer - got stuck on how to have the setTimeout callback be a function within an anonymous function!
+
+    delayClickHelper = (e) => {
+        e.persist()
+        
+        window.setTimeout(() => {this.props.onDelayedClick(e)}, this.props.delay)
+        
+    }
+    
+    
+    render(){
+        return( <button onClick={this.delayClickHelper}> Delayed Button </button>)
+    }
+
+}
+
+export default DelayedButton
+
+
+//where I got to before checking solution:
+
+// delayClickHelper = (e) => {
+//     e.persist()
+    
+//     window.setTimeout(this.props.onDelayedClick(e), this.props.delay)
+    
+// }
+
+
+// render(){
+//     return( <button onClick={this.delayClickHelper}> Delayed Button </button>)
+// }
+
+// }
