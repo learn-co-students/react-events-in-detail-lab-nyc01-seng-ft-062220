a3dfb8023f3681d9dd235e4ef55987437d693339
Luis Reyes <luisreyesxv@yahoo.com>
2020-08-27 02:56:35 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,28 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+
+export default class DelayedButton extends React.Component{
+
+    mouseHandler = (event) =>{
+        event.persist()
+        setTimeout(this.props.onDelayedClick,this.props.delay,event)
+
+    }
+
+
+    render(){
+
+        return (
+            
+            <button onClick={this.mouseHandler} > Delay Swag</button>
+
+
+        )
+
+
+    }
+
+
+}
+
